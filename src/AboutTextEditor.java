d83e35b1561880f6ad4b799cf4fdc3847258cc61
Stewart Malik <mali0037@gmail.com>
2012-07-24 20:35:01 +0930

    Up to Task 21 (Adding Save and Open Dialogs)

--- src/AboutTextEditor.java
+++ src/AboutTextEditor.java
@@ -1,8 +1,13 @@
 import java.awt.*;
 import java.awt.event.*;
+
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
 
 
 @SuppressWarnings("serial")
@@ -14,19 +19,45 @@ public class AboutTextEditor extends JDialog implements ActionListener {
 	JPanel buttonPane = new JPanel();
 	FlowLayout buttonLayout = new FlowLayout();
 	
+	ImageIcon aboutIcon;
+	JLabel graphicLabel = new JLabel();
+	
+	JPanel infoPane = new JPanel();
+	
 	JButton buttonOK = new JButton("Ok");
 	
 	public AboutTextEditor(Frame owner) {
 		super(owner);
 		
+		this.setTitle("About");
+		this.setResizable(false);
+		
 		contentPane = (JPanel) this.getContentPane();
 		contentPane.setLayout(contentLayout);
+		
+		try {
+			aboutIcon = new ImageIcon(AboutTextEditor.class.getResource("about.gif"));
+			graphicLabel.setIcon(aboutIcon);
+		} catch (Exception e) {
+			graphicLabel.setText("TextEditor");
+		}
+		graphicLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		infoPane.setLayout(new BoxLayout(infoPane, BoxLayout.Y_AXIS));
+		infoPane.add(new JLabel("TextEditor"));
+		infoPane.add(new JLabel("A simple Text Editor using Swing"));
+		infoPane.add(new JLabel("Version 1.0"));
+		infoPane.add(new JLabel("Author: Stewart Malik"));
+		infoPane.add(new JLabel("Copyright(c) 2012 WTFPL"));
+		infoPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		
 		buttonPane.setLayout(buttonLayout);
 		buttonPane.add(buttonOK);
 		
 		buttonOK.addActionListener(this);
 		
+		contentPane.add(graphicLabel, BorderLayout.WEST);
+		contentPane.add(infoPane, BorderLayout.EAST);
 		contentPane.add(buttonPane, BorderLayout.SOUTH);
 		
 		this.pack();
